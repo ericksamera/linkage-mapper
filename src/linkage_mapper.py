64bfb241a63fa6f5a75b34e2cc60d26210eb8f97
Erick Samera <90236200+ericksamera@users.noreply.github.com>
2022-12-08 15:51:03 -0800

    fix: report?

--- src/linkage_mapper.py
+++ src/linkage_mapper.py
@@ -65,7 +65,7 @@ def _identify_chromosome(_key: str, _value: dict):
     
     with open('blast_output.json', 'r') as json_file:
         blast_object = json.load(json_file)
-        for i in blast_object['results']['search']['hits']:
+        for i in blast_object['report']['results']['search']['hits']:
             print(i)
 
 
